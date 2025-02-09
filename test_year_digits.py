9b943a475194008081bd0cb71702410d59146f1f
Dave Potts <dave@davepotts.software>
2025-02-09 15:56:45 +0000

    Started implementing the calculator

--- test_year_digits.py
+++ test_year_digits.py
@@ -1,6 +1,15 @@
 
 
 def test_2025_0():
-    sum = "0 = 0 * (2  + 2 + 5)"
+    year_calcaulator = YearDigits(2025)
 
-    assert sum == "0 = 0 * (2  + 2 + 5)"
+    assert year_calcaulator.calculate_for(0) == "0 = 0 * (2  + 2 + 5)"
+
+
+class YearDigits:
+    """For a given year, show how to calulate an integer using all the digits of the year"""
+    def __init__(self, year):
+        self.year = year
+
+    def calculate_for(self, digit):
+        return "0 = 0 * (2  + 2 + 5)"
