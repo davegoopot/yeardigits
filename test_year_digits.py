9186e9de33f2b6ec74e07e8096e1626470ca94b2
Dave Potts <dave@davepotts.software>
2025-02-22 20:23:28 +0000

    Test for next didgit

--- test_year_digits.py
+++ test_year_digits.py
@@ -6,6 +6,11 @@ def test_2025_0():
     assert year_calcaulator.calculate_for(0) == "0 = 0 * (2  + 2 + 5)"
 
 
+def test_2025_1():
+    year_calcaulator = YearDigits(2025)
+
+    assert year_calcaulator.calculate_for(1) == "1 = 5 - 2 - 2 + 0"
+
 class YearDigits:
     """For a given year, show how to calulate an integer using all the digits of the year"""
     def __init__(self, year):
