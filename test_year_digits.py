ce691ee3a0e3d7051af54d487f15e12beed40586
Dave Potts <dave@goopot.co.uk>
2025-03-05 21:11:25 +0000

    Implement 1 for Year 1
    Test for 1 = 1

--- test_year_digits.py
+++ test_year_digits.py
@@ -1,9 +1,13 @@
-
-
 def test_2025_0():
     year_calcaulator = YearDigits(2025)
 
     assert year_calcaulator.calculate_for(0) == "0 = 0 * (2  + 2 + 5)"
+
+
+def test_year_1():
+    year_calculator = YearDigits(1)
+
+    assert year_calculator.calculate_for(1) == "1 = 1"
 
 
 class YearDigits:
