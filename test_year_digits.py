fc4cf65615239404fe004c8718daa625b59bf01f
Dave Potts <dave@davepotts.software>
2025-03-05 21:18:49 +0000

    Base case: if the target matches the year then return target = year

--- test_year_digits.py
+++ test_year_digits.py
@@ -1,9 +1,13 @@
-
-
 def test_2025_0():
     year_calcaulator = YearDigits(2025)
 
     assert year_calcaulator.calculate_for(0) == "0 = 0 * (2  + 2 + 5)"
+
+
+def test_year_1():
+    year_calculator = YearDigits(1)
+
+    assert year_calculator.calculate_for(1) == "1 = 1"
 
 
 class YearDigits:
@@ -12,4 +16,7 @@ class YearDigits:
         self.year = year
 
     def calculate_for(self, digit):
-        return "0 = 0 * (2  + 2 + 5)"
+        if digit == self.year:
+            return f"{digit} = {digit}"
+        else:
+            return "0 = 0 * (2  + 2 + 5)"
